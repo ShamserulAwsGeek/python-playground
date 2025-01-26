f52e62e26cc54e03b753aa474dcbfa54911cf3d4
ShamserulAwsGeek <shamserul786@gmail.com>
2025-01-26 18:31:06 +0530

    new file added

--- DAY-02/string-lowercase.py
+++ DAY-02/string-lowercase.py
@@ -1,5 +1,7 @@
 text = "python is awesome !"
 uppercase = text.upper()
 lowercase = text.lower()
+camelcase = text.title()
 print(uppercase)
 print(lowercase)
+print(camelcase)
---
+++ DAY-02/string-replace.py
@@ -0,0 +1,3 @@
+text = "python is a programming language"
+new_text = text.replace("programming", "scripting")
+print("modified text:", new_text) 
