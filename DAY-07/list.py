6434d27be6f5886066567c53945f320896e0fb37
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-03 21:09:11 +0530

    list added

---
+++ DAY-07/list.py
@@ -0,0 +1,61 @@
+list in python:
+    - list is a collection of items.
+    - list is a mutable data type.
+    - list is a ordered
+
+# Creating a list:
+    - list can be created by using [] or list() constructor.
+    - list can contain any type of data type.
+    - list can contain duplicate values.
+    - list can contain nested list.
+    - list can contain different data types.
+    - list can contain mutable data types.
+
+# Accessing elements in list:
+    - list can be accessed by using index.
+    - index starts from 0.
+    - index can be negative.
+    - index can be slice
+    
+# Updating elements in list:
+# Deleting elements in list:
+# List methods:
+# List operations:
+# List comprehension:
+# List functions:
+#   - len()
+#  - min()
+# - max()
+# - sum()
+# - sorted()
+# - reversed()
+# - any()
+# - all()
+# - enumerate()
+# - zip()
+# - filter()
+# - map()
+# - reduce()
+# - lambda()
+# - list()
+# - tuple()
+# - set()
+# - dict()
+# - frozenset()
+# - range()
+# - xrange()
+# - type()
+# - id()
+# - isinstance()
+# - issubclass()
+# - getattr()
+# - setattr()
+# - delattr()
+# - hasattr()
+# - vars()
+# - locals()
+# - globals()
+# - dir()
+# - help()
+# - input()
+    
