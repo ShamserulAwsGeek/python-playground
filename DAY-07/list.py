d3b4168af993f8c79d4b437423f4ea75d129ddfc
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-04 17:42:52 +0530

    list added

--- DAY-07/list.py
+++ DAY-07/list.py
@@ -1,21 +1,21 @@
-list in python:
-    - list is a collection of items.
-    - list is a mutable data type.
-    - list is a ordered
+# List in Python:
+# - list is a collection of items.
+# - list is a mutable data type.
+# - list is ordered
 
 # Creating a list:
-    - list can be created by using [] or list() constructor.
-    - list can contain any type of data type.
-    - list can contain duplicate values.
-    - list can contain nested list.
-    - list can contain different data types.
-    - list can contain mutable data types.
+# - list can be created by using [] or list() constructor.
+# - list can contain any type of data type.
+# - list can contain duplicate values.
+# - list can contain nested list.
+# - list can contain different data types.
+# - list can contain mutable data types.
 
 # Accessing elements in list:
-    - list can be accessed by using index.
-    - index starts from 0.
-    - index can be negative.
-    - index can be slice
+# - list can be accessed by using index.
+# - index starts from 0.
+# - index can be negative.
+# - index can be slice
     
 # Updating elements in list:
 # Deleting elements in list:
@@ -58,4 +58,8 @@ list in python:
 # - dir()
 # - help()
 # - input()
-    
+
+
+servers = list(("web-server-01", "db-server-01", "app-server-01"))
+print(servers[0])
+#print(type(servers))
