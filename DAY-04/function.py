145ff725eba05e75b7c7103239f58c4b216074e6
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-04 20:31:44 +0530

    function added

--- DAY-04/function.py
+++ DAY-04/function.py
@@ -7,23 +7,55 @@ print(message)
 # Modules are Python files that consist of Python code. This code can either be functions classes or variables. A Python module is a .py file containing executable code.
 
 #my_module.py
-def square(x):
-    return x ** 2
+# def square(x):
+#     return x ** 2
 
-pi = 3.14159265
+# pi = 3.14159265
 
 # Importing Modules
-import my_module # type: ignore
-print(my_module.square(3))
-print(my_module.pi)
+# import my_module # type: ignore
+# print(my_module.square(3))
+# print(my_module.pi)
 
 #import the entire module:
-import math
+#import math
 
 #use functions/variables from the module:
-math.sqrt(16)
+#math.sqrt(16)
 
 #import specifiv functions/variables from the module:
-from math import sqrt
-print(sqrt(16))
+# from math import sqrt
+# print(sqrt(16))
 
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def sham(name):
+    print("Sham " + name)
+sham("Maheen")
+
+x = 10
+y = 20
+
+def add():
+    add = x + y
+    print(add)
+def sub():
+    sub = x - y
+    print(sub)
+def mul():
+    mul = x * y
+    print(mul)
+def div():
+    div = x / y
+    print(div)
+
+add()
+sub()
+mul()
+div()
