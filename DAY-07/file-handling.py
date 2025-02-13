73c91441b2a862c80537148b9d763c26174bce14
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-13 19:51:29 +0530

    added file handling

--- DAY-07/file-handling.py
+++ DAY-07/file-handling.py
@@ -4,3 +4,21 @@ print(f.read())
 
 f = open("demofile.txt", "r")
 print(f.read(10))
+
+f = open("demofile.txt", "rt")
+print(f.readline())
+
+
+f = open("demofile.txt", "r")
+print(f.readline())
+print(f.readline())
+
+
+f = open("demofile.txt", "r")
+for x in f:
+  print(x)
+
+
+f = open("demofile.txt", "r")
+print(f.readline())
+f.close()
