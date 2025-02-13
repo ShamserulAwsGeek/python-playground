3822963082f1d8cd756fe7a321d12b83c3a84d48
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-13 20:07:11 +0530

    added file handling

--- DAY-07/file-handling.py
+++ DAY-07/file-handling.py
@@ -1,24 +1,57 @@
-f = open("demofile.txt")
-f = open("demofile.txt", "rt")
-print(f.read())
+# f = open("demofile.txt")
+# f = open("demofile.txt", "rt")
+# print(f.read())
+
+# f = open("demofile.txt", "r")
+# print(f.read(10))
+
+# f = open("demofile.txt", "rt")
+# print(f.readline())
+
+
+# f = open("demofile.txt", "r")
+# print(f.readline())
+# print(f.readline())
+
+
+# f = open("demofile.txt", "r")
+# for x in f:
+#   print(x)
+
+
+# f = open("demofile.txt", "r")
+# print(f.readline())
+# f.close()
+
+# f = open("demofile.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+
+#open and read the file after the appending:
+# f = open("demofile.txt", "r")
+# print(f.read())
+
+
+# f = open("demofile.txt", "w")
+# f.write("Woops! I have deleted the content!")
+# f.close()
+
+#open and read the file after the overwriting:
+# f = open("demofile.txt", "r")
+# print(f.read())
+
+
 
 f = open("demofile.txt", "r")
-print(f.read(10))
-
-f = open("demofile.txt", "rt")
 print(f.readline())
 
 
-f = open("demofile.txt", "r")
-print(f.readline())
-print(f.readline())
+# import os
+# os.add("demofile.txt")
 
+# import os
+# if os.path.exists("demofile.txt"):
+#   os.remove("demofile.txt")
+# else:
+#   print("The file does not exist")
 
-f = open("demofile.txt", "r")
-for x in f:
-  print(x)
-
-
-f = open("demofile.txt", "r")
-print(f.readline())
-f.close()
