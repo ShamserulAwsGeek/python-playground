68b7d7f929fcfb1c1bbb6b075cd31ebd9022f2fc
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-15 17:06:48 +0530

    added some string files

--- DAY-02/string-modify.py
+++ DAY-02/string-modify.py
@@ -1,2 +1,28 @@
 a = "Hello, World!"
 print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+a = "Hello, World!"
+print(a.split(","))
+
+age = 29
+txt = f"my name is Shamserul and my current age is {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
