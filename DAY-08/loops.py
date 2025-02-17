7e3916ccebcb4eb577966ff0c961706989d8af00
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-17 23:06:04 +0530

    loops added

---
+++ DAY-08/loops.py
@@ -0,0 +1,18 @@
+for i in range(4):
+    print("Sham Loves Maheen")
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    if number == 3:
+        break
+    print(number)
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    if number == 3:
+        continue
+    print(number)
