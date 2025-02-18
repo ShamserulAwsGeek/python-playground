7529511a70164a14261c62db52d90f99b6ce461f
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-18 18:52:17 +0530

    shell script added

--- DAY-08/while.sh
+++ DAY-08/while.sh
@@ -21,3 +21,18 @@ while true; do
     fi
     sleep 30
 done
+
+while true; do
+    if tail -n 1 /var/log/app.log | grep -q "ERROR"; then
+        send_alert "Error detected in the log."
+    fi
+    sleep 5
+done
+
+while true; do
+    replication_lag=$(mysql -e "SHOW SLAVE STATUS\G" | grep "Seconds_Behind_Master" | awk '{print $2}')
+    if [ "$replication_lag" -gt 60 ]; then
+        trigger_data_sync
+    fi
+    sleep 60
+done
