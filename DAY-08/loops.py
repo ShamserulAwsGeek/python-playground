7ff52b16de8d2f1fde189997142f270e6360f75f
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-18 17:03:11 +0530

    added loops

--- DAY-08/loops.py
+++ DAY-08/loops.py
@@ -1,18 +1,40 @@
-for i in range(4):
-    print("Sham Loves Maheen")
+# for i in range(4):
+#     print("Sham Loves Maheen")
 
-fruits = ["apple", "banana", "cherry"]
-for fruit in fruits:
-    print(fruit)
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
 
-numbers = [1, 2, 3, 4, 5]
-for number in numbers:
-    if number == 3:
-        break
-    print(number)
+# numbers = [1, 2, 3, 4, 5]
+# for number in numbers:
+#     if number == 3:
+#         break
+#     print(number)
 
-numbers = [1, 2, 3, 4, 5]
-for number in numbers:
-    if number == 3:
-        continue
-    print(number)
+# numbers = [1, 2, 3, 4, 5]
+# for number in numbers:
+#     if number == 3:
+#         continue
+#     print(number)
+
+#while loop:
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+
+# log_file = [
+#    "INFO: Operation successful",
+#    "ERROR: File not found",
+#    "DEBUG: Connection established",
+#    "ERROR: Database connection failed",
+# ]
+
+# for line in log_file:
+#    if "ERROR" in line:
+#        print(line)
+
+servers=("server1" "server2" "server3")
+for server in "${servers[@]}": 
+    #configure_monitoring_agent "$server"
+    print(server)
