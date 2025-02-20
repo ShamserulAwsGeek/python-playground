2cf3d69c9f3d8444ea29ea374ff6ed37456c0475
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-20 18:28:19 +0530

    dictionary added

--- DAY-10/api-call.py
+++ DAY-10/api-call.py
@@ -1,8 +1,11 @@
-import requests
+# import requests
  
-response = requests.get("https://api.github.com/repos/aws-samples/amazon-bedrock-workshop/pulls")
+# response = requests.get("https://api.github.com/repos/aws-samples/amazon-bedrock-workshop/pulls")
 
-compelte_detail = response.json()
+# compelte_detail = response.json()
 
-for i in range(len(compelte_detail)):
- print(compelte_detail[i]["user"]["login"])
+# for i in range(len(compelte_detail)):
+#  print(compelte_detail[i]["user"]["login"])
+
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+print(my_dict['name'])  # Output: John
---
+++ DAY-10/dictionary.py
@@ -0,0 +1,2 @@
+my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
+print(my_dict['name'])  # Output: John
