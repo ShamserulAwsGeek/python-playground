7ff7842f428f75e0b34dfb3709bed83e203389e7
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-25 23:01:47 +0530

    get object

--- DAY-13/aws-api-lab.py
+++ DAY-13/aws-api-lab.py
@@ -3,7 +3,38 @@ import boto3
 client = boto3.client('s3')
 
 
-response = client.create_bucket(  
+# response = client.create_bucket(  
+#     Bucket='sham-s3-boto3-lab',
+    
+# )
+
+response = client.get_bucket_acl(
     Bucket='sham-s3-boto3-lab',
     
+)
+
+print(response)
+
+
+response = client.get_object(
+    Bucket='string',
+    IfMatch='string',
+    IfModifiedSince=datetime(2015, 1, 1),
+    IfNoneMatch='string',
+    IfUnmodifiedSince=datetime(2015, 1, 1),
+    Key='string',
+    Range='string',
+    ResponseCacheControl='string',
+    ResponseContentDisposition='string',
+    ResponseContentEncoding='string',
+    ResponseContentLanguage='string',
+    ResponseContentType='string',
+    ResponseExpires=datetime(2015, 1, 1),
+    VersionId='string',
+    SSECustomerAlgorithm='string',
+    SSECustomerKey='string',
+    RequestPayer='requester',
+    PartNumber=123,
+    ExpectedBucketOwner='string',
+    ChecksumMode='ENABLED'
 )
