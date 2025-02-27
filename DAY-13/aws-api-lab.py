666bbe6262bfe7c4fb8da0a1ec9caddec3c1767f
ShamserulAwsGeek <shamserul786@gmail.com>
2025-02-27 16:00:57 +0530

    updated the folder

--- DAY-13/aws-api-lab.py
+++ DAY-13/aws-api-lab.py
@@ -15,26 +15,3 @@ response = client.get_bucket_acl(
 
 print(response)
 
-
-response = client.get_object(
-    Bucket='string',
-    IfMatch='string',
-    IfModifiedSince=datetime(2015, 1, 1),
-    IfNoneMatch='string',
-    IfUnmodifiedSince=datetime(2015, 1, 1),
-    Key='string',
-    Range='string',
-    ResponseCacheControl='string',
-    ResponseContentDisposition='string',
-    ResponseContentEncoding='string',
-    ResponseContentLanguage='string',
-    ResponseContentType='string',
-    ResponseExpires=datetime(2015, 1, 1),
-    VersionId='string',
-    SSECustomerAlgorithm='string',
-    SSECustomerKey='string',
-    RequestPayer='requester',
-    PartNumber=123,
-    ExpectedBucketOwner='string',
-    ChecksumMode='ENABLED'
-)
