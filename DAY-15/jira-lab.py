9d6b540ceade4490382b6196df96dbf6000c59c7
Md Shamserul Haque <44093151+ShamserulAwsGeek@users.noreply.github.com>
2025-02-28 00:14:57 +0530

    Update jira-lab.py

--- DAY-15/jira-lab.py
+++ DAY-15/jira-lab.py
@@ -1,8 +1,3 @@
-
-
-#ATATT3xFfGF0QxZC-q1VbMel8wQEkvs0N8IKS70-cV0E2gJ1JOBay2Q_ePQH3isH-jebq4ppXIqAN9OIKN-BMz_LSM9a3oyLdCM-lGaJ_6rfm00geuUOL8egysiFE2uTNykaHE-rSoi4qSsLqwmQmMo1UXILiwwQgCJyS2emP3Zh4_zKWAVoOs8=7DE75A18
-
-
 # This code sample uses the 'requests' library:
 # http://docs.python-requests.org
 import requests
@@ -11,7 +6,7 @@ import json
 
 url = "https://shamserul1357.atlassian.net/rest/api/3/project"
 
-auth = HTTPBasicAuth("shamserul1357@gmail.com", "ATATT3xFfGF0QxZC-q1VbMel8wQEkvs0N8IKS70-cV0E2gJ1JOBay2Q_ePQH3isH-jebq4ppXIqAN9OIKN-BMz_LSM9a3oyLdCM-lGaJ_6rfm00geuUOL8egysiFE2uTNykaHE-rSoi4qSsLqwmQmMo1UXILiwwQgCJyS2emP3Zh4_zKWAVoOs8=7DE75A18")
+auth = HTTPBasicAuth("shamserul1357@gmail.com", "api-token")
 
 headers = {
   "Accept": "application/json"
