368448375febbd5aaf801efd156f2192bc154343
ShamserulAwsGeek <shamserul786@gmail.com>
2025-03-04 15:40:20 +0530

    added boto3 lab

--- DAY-14/boto-lab.py
+++ DAY-14/boto-lab.py
@@ -1,23 +1,7 @@
-# This code sample uses the 'requests' library:
-# http://docs.python-requests.org
-import requests
-from requests.auth import HTTPBasicAuth
-import json
+import boto3
 
-url = "https://shamserul1357.atlassian.net//rest/api/3/project"
-
-auth = HTTPBasicAuth("email@example.com", "<api_token>")
-
-headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json"
-}
-
-response = requests.request(
-   "POST",
-   url,
-   headers=headers,
-   auth=auth
+client = boto3.client('s3')
+response = client.create_bucket(
+    Bucket='sham-boto3-bucket',
+    
 )
-
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
