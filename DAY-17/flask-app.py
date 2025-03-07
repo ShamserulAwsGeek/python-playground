dbe39de8b0e0924df4f9fb2448c682a633ba2273
ShamserulAwsGeek <shamserul786@gmail.com>
2025-03-07 22:46:21 +0530

    added some context to the flask app

--- DAY-17/flask-app.py
+++ DAY-17/flask-app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #this is decorator to route the URL to the function
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=8000)
+if __name__ == '__main__': #this is to run the app on localhost on port 8000 if the file is run directly and not imported as a module in another file 
+  app.run(debug=True, host='0.0.0.0', port=8000) #this is to run the app on localhost on port 8000
